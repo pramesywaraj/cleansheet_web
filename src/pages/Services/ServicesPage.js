a593e9a41b4e9f8074f6e3e7cf432be745800104
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-03-06 15:57:16 +0700

    making services layout

--- src/pages/Services/ServicesPage.js
+++ src/pages/Services/ServicesPage.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import ServicesPageStyle from './servicesPage.module.scss';
+
 export default function ServicesPage() {
-  return <div />;
+  return (
+    <div className={ServicesPageStyle['services-wrapper']}>
+      <div>Header</div>
+      <div>Content</div>
+    </div>
+  );
 }
