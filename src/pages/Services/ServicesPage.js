4cd8fd219a35b67f9496eec5c4d576d9e720f45e
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-03-08 11:37:35 +0700

    separating the component

--- src/pages/Services/ServicesPage.js
+++ src/pages/Services/ServicesPage.js
@@ -5,6 +5,39 @@ import TabsContainer from '../../components/Tabs/TabsContainer';
 import Loading from '../../components/Loading/Loading';
 import ServiceCard from '../../components/Cards/ServiceCard';
 
+const Cleaning = () => {
+  return (
+    <div className={ServicesPageStyle['services-container']}>
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+    </div>
+  );
+};
+
+const Washing = () => {
+  return (
+    <div className={ServicesPageStyle['services-container']}>
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+    </div>
+  );
+};
+
+const EnvirontmentClean = () => {
+  return (
+    <div className={ServicesPageStyle['services-container']}>
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+    </div>
+  );
+};
+
 export default function ServicesPage() {
   const [activePanel, setActivePanel] = useState(0);
 
@@ -17,13 +50,9 @@ export default function ServicesPage() {
   return (
     <div className={ServicesPageStyle['services-wrapper']}>
       <TabsContainer changeActivePanel={changeActivePanel} />
-      <div className={ServicesPageStyle['services-container']}>
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-        <ServiceCard />
-      </div>
+      {activePanel === 0 && <Cleaning />}
+      {activePanel === 1 && <Washing />}
+      {activePanel === 2 && <EnvirontmentClean />}
     </div>
   );
 }
