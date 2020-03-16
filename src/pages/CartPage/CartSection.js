87a682cb46e27a612b0cc358920f0e24667b8bd0
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-03-16 23:33:43 +0700

    cart item prototype

--- src/pages/CartPage/CartSection.js
+++ src/pages/CartPage/CartSection.js
@@ -3,11 +3,28 @@ import React from 'react';
 import CartStyle from './cart.module.scss';
 import CartCard from '../../components/Cards/CartCard';
 
+import ProductImagePath from '../../assets/product_pictures/prod2.png';
+
 export default function CartSection() {
   return (
     <CartCard label="Keranjang Belanja">
-      <div className={`${CartStyle['']}`}>
-        <div />
+      <div className={`${CartStyle['cart-item-container']}`}>
+        <div className={`${CartStyle['cart-item-row']}`}>
+          <div className={`${CartStyle['cart-item-image']}`}>
+            <img src={ProductImagePath} alt="Image" />
+          </div>
+          <div className={`${CartStyle['cart-item-detail']}`}>
+            <p className={`${CartStyle['item-price']}`}>Sapu Lidi Kencrot</p>
+            <div className={`${CartStyle['cart-item-price-delete']}`}>
+              <div>price</div>
+              <div>delete</div>
+            </div>
+          </div>
+        </div>
+        <div className={`${CartStyle['cart-item-row']} ${CartStyle['cart-item-button']}`}>
+          <div>Button</div>
+          <div>Total Price per Item</div>
+        </div>
       </div>
       <div className={`${CartStyle['cart-total']}`}>
         <div className={`${CartStyle['flex-row']}`}>
