d70def13cc1e17114576e32d8a0c2f211777f35a
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-03-21 12:00:35 +0700

    fix shopping cart layout

--- src/pages/CartPage/CartSection.js
+++ src/pages/CartPage/CartSection.js
@@ -9,24 +9,27 @@ import ProductImagePath from '../../assets/product_pictures/prod2.png';
 export default function CartSection() {
   return (
     <CartCard label="Keranjang Belanja">
-      <div className={`${CartStyle['cart-item-container']}`}>
-        <div className={`${CartStyle['cart-item-row']}`}>
-          <div className={`${CartStyle['cart-item-image']}`}>
-            <img src={ProductImagePath} alt="Image" />
-          </div>
-          <div className={`${CartStyle['cart-item-detail']}`}>
-            <p className={`${CartStyle['item-price']}`}>Sapu Lidi Kencrot</p>
-            <div className={`${CartStyle['cart-item-price-delete']}`}>
-              <p>Rp. 15.000 /pcs</p>
-              <IconButton iconColor="#a1a1a1" label={<FaTrash />} />
+      <div className={`${CartStyle['cart-item-list']}`}>
+        <div className={`${CartStyle['cart-item-container']}`}>
+          <div className={`${CartStyle['cart-item-row']}`}>
+            <div className={`${CartStyle['cart-item-image']}`}>
+              <img src={ProductImagePath} alt="Image" />
+            </div>
+            <div className={`${CartStyle['cart-item-detail']}`}>
+              <p className={`${CartStyle['item-price']}`}>Sapu Lidi Kencrot</p>
+              <div className={`${CartStyle['cart-item-price-delete']}`}>
+                <p>Rp. 15.000 /pcs</p>
+                <IconButton iconColor="#a1a1a1" label={<FaTrash />} />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={`${CartStyle['cart-item-row']} ${CartStyle['cart-item-button']}`}>
-          <NumberOfGoods />
-          <p>Rp. 15.000</p>
+          <div className={`${CartStyle['cart-item-row']} ${CartStyle['cart-item-button']}`}>
+            <NumberOfGoods />
+            <p>Rp. 15.000</p>
+          </div>
         </div>
       </div>
+
       <div className={`${CartStyle['cart-total']}`}>
         <div className={`${CartStyle['flex-row']}`}>
           <p>Total</p>
