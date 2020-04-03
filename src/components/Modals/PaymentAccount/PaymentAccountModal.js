94f059a05b1779070e05d6fdaaae21369826deee
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-03 11:15:45 +0700

    payment avail item list styling done

--- src/components/Modals/PaymentAccount/PaymentAccountModal.js
+++ src/components/Modals/PaymentAccount/PaymentAccountModal.js
@@ -9,6 +9,9 @@ export default function PaymentAccountModal({ show, close }) {
   return (
     <ModalBase show close={close} title="Pembayaran">
       <div className={`${PaymentModalStyle['modal-payment-container']}`}>
+        <div className={PaymentModalStyle['modal-payment-sub']}>
+          <p>Anda dapat melakukan pembayaran melalui: </p>
+        </div>
         <div className={`${PaymentModalStyle['modal-payment-list']}`}>
           <PaymentItem />
         </div>
