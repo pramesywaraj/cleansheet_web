0c549697dc02de3669d6fa2d06988189f9ea4c4e
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-06 15:16:47 +0700

    modify the tab

--- src/components/Tabs/TabsContainer.js
+++ src/components/Tabs/TabsContainer.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useLocation } from 'react-router-dom';
 
 import TabsStyle from './tabs.module.scss';
 import Tab from './Tab';
 
-import { TABTYPE } from '../../hooks/useTabs';
+import { TABTYPE, TRANSACTIONTAB } from '../../hooks/useTabs';
 
 export default function TabsContainer({ handleTabChange, activeTab }) {
+  const [tabObj, setTab] = useState({});
+  const { pathname } = useLocation();
+
+  function checkPathname() {
+    switch (pathname) {
+      case '/layanan':
+        setTab(TABTYPE);
+        break;
+      case '/transaksi':
+        setTab(TRANSACTIONTAB);
+        break;
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => {
+    checkPathname();
+  }, []);
+
   return (
     <div className={TabsStyle['tabs-container']}>
       <div className={TabsStyle['tabs-content']}>
-        {Object.keys(TABTYPE).map((key, index) => (
+        {Object.keys(tabObj).map((key, index) => (
           <Tab
-            key={`${TABTYPE[key].code}-tab`}
-            label={TABTYPE[key].name}
-            selected={TABTYPE[key].code === activeTab}
-            onSelect={() => handleTabChange(TABTYPE[key].code)}
+            key={`${tabObj[key].code}-tab`}
+            label={tabObj[key].name}
+            selected={tabObj[key].code === activeTab}
+            onSelect={() => handleTabChange(tabObj[key].code)}
           />
         ))}
       </div>
--- src/pages/Transaction/TransactionPage.js
+++ src/pages/Transaction/TransactionPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import useTabs, { TABTYPE } from '../../hooks/useTabs';
+import useTabs, { TRANSACTIONTAB } from '../../hooks/useTabs';
 
 import TransactionStyle from './transaction.module.scss';
 
 import TabsContainer from '../../components/Tabs/TabsContainer';
 
 export default function TransactionPage() {
-  const [activeTab, onChangeTab] = useTabs(TABTYPE.sanitation.code);
+  const [activeTab, onChangeTab] = useTabs(TRANSACTIONTAB.service.code);
+
+  function handleTabChange(key) {
+    if (activeTab === key) return;
+    onChangeTab(key);
+  }
 
   return (
     <div className={TransactionStyle['transaction-wrapper']}>
