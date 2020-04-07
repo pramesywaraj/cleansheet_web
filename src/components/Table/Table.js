3d1b071f6161b5b349749328cb5496f10321da73
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-07 11:01:53 +0700

    add style to the table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,20 +7,45 @@ export default function Table() {
     <div className={TableStyle['table-wrapper']}>
       <div className={TableStyle['table-container']}>
         <table>
-          <tr>
-            <th>Tanggal Pemesanan</th>
-            <th>Kode Pesanan</th>
-            <th>Total Harga</th>
-            <th>Status Pemesanan</th>
-            <th> </th>
-          </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>Germany</td>
-            <td>Lihat detail</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Tanggal Pemesanan</th>
+              <th>Kode Pesanan</th>
+              <th>Total Harga</th>
+              <th>Status Pemesanan</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+              <td>Germany</td>
+              <td>Lihat detail</td>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+              <td>Germany</td>
+              <td>Lihat detail</td>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+              <td>Germany</td>
+              <td>Lihat detail</td>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+              <td>Germany</td>
+              <td>Lihat detail</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
