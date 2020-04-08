c343aa7f510046c9293c1d3f7b4f9e4bb3a6bb36
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-08 15:29:30 +0700

    modal style done

--- src/components/Modals/Transaction/TransactionDetailModal.js
+++ src/components/Modals/Transaction/TransactionDetailModal.js
@@ -5,12 +5,60 @@ import TransactionModalStyle from './transactionModal.module.scss';
 import ModalBase from '../ModalBase';
 
 export default function TransactionDetailModal({ data, show, close, title }) {
+  const renderTransactionDetail = () => (
+    <div className={TransactionModalStyle['modal-transaction-detail']}>
+      <div className={TransactionModalStyle['transaction-detail-value']}>
+        <div>
+          <p>Tanggal Pemesananan </p>
+          <p>:</p>
+          <p>6 Desember 2019</p>
+        </div>
+        <div>
+          <p>Status Pemesananan </p>
+          <p>:</p>
+          <p>Belum dibayar</p>
+        </div>
+        <div>
+          <p>Batas Pembayaran </p>
+          <p>:</p>
+          <p>7 Desember 2019</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const renderProductItem = () => (
+    <div className={TransactionModalStyle['transaction-products-list']}>
+      <div className={TransactionModalStyle['product-item']}>
+        <p>1</p>
+        <p>Ijuk</p>
+        <p>x 2</p>
+      </div>
+      <div className={TransactionModalStyle['product-item']}>
+        <p>1</p>
+        <p>Ijuk</p>
+        <p>x 2</p>
+      </div>
+      <div className={TransactionModalStyle['product-item']}>
+        <p>1</p>
+        <p>Ijuk</p>
+        <p>x 2</p>
+      </div>
+    </div>
+  );
+
   return (
     <ModalBase show={show} close={close} title={title}>
       <div className={TransactionModalStyle['modal-transaction-container']}>
-        <div />
-        <div />
-        <div />
+        <div className={TransactionModalStyle['modal-transaction-total_charge']}>
+          <p>Total yang harus dibayar</p>
+          <p>Rp. 300.000, -</p>
+        </div>
+        {renderTransactionDetail()}
+        <div className={TransactionModalStyle['modal-transaction-products_container']}>
+          <p>Yang dipesan :</p>
+          {renderProductItem()}
+        </div>
       </div>
     </ModalBase>
   );
--- src/pages/Transaction/TransactionPage.js
+++ src/pages/Transaction/TransactionPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import useTabs, { TRANSACTIONTAB } from '../../hooks/useTabs';
 import useModal from '../../hooks/useModal';
