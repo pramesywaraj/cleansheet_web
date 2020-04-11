e0df113b39eaf2700abe5969903db948413e00e2
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-11 18:51:57 +0700

    integrating fetch products api done

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
+import useFetchData from '../../hooks/useFetchData';
 
 import MainSection from './MainSection';
 import ServiceSection from './ServiceSection';
@@ -10,6 +11,8 @@ import JoinCleansheetPartner from './JoinCleansheetPartner';
 
 export default function LandingPage() {
   const history = useHistory();
+  const { loading, response } = useFetchData('master/products?item_per_page=4');
+  const [products, setProducts] = useState([]);
 
   const goToProductPage = () => {
     history.push('/produk');
@@ -19,11 +22,15 @@ export default function LandingPage() {
     history.push('/layanan');
   };
 
+  useEffect(() => {
+    setProducts(response.data.products);
+  }, [response]);
+
   return (
     <>
       <MainSection goToProductPage={goToProductPage} />
       <ServiceSection goToServicePage={goToServicePage} />
-      <ProductSection goToProductPage={goToProductPage} />
+      <ProductSection goToProductPage={goToProductPage} loading={loading} products={products} />
       <ClientSection />
       <JoinCleansheetWorkerSection />
       <JoinCleansheetPartner />
--- src/pages/LandingPage/ProductSection.js
+++ src/pages/LandingPage/ProductSection.js
@@ -4,17 +4,37 @@ import LandingPageStyle from './landingPage.module.scss';
 
 import ProductCard from '../../components/Cards/ProductCard';
 import OutlinedButton from '../../components/Buttons/OutlinedButton';
+import Loading from '../../components/Loading/Loading';
 
-export default function ProductSection({ goToProductPage, products }) {
+export default function ProductSection({ goToProductPage, products, loading }) {
+  console.log(products);
+  const renderProducts = () => {
+    if (products.length === 0) {
+      return (
+        <div className="grid-item-20">
+          <p>Tidak ada produk untuk ditampilkan.</p>
+        </div>
+      );
+    }
+
+    return products.map(item => (
+      <div key={item.id} className="grid-item-20">
+        <ProductCard
+          imgSrc={item.image_url}
+          productName={item.name}
+          price={item.price}
+          addProduct={goToProductPage}
+        />
+      </div>
+    ));
+  };
   return (
     <section className="section-gap">
       <div className="text-center">
         <h1>Produk Kami</h1>
         <p>Dapatkan produk unggulan kami dalam bidang kebersihan.</p>
         <div className={`grid-container ${LandingPageStyle['product-grid']}`}>
-          <div className="grid-item-20">
-            <ProductCard imgSrc="" productName="Sapu Kebersihan" price="15.000" />
-          </div>
+          {loading ? <Loading /> : renderProducts()}
         </div>
         <div className={`${LandingPageStyle.buttonMargin5}`}>
           <OutlinedButton clickAction={goToProductPage} label="Selengkapnya" />
