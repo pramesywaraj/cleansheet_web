0898bc21709f99f0da1972dcbc170789eaf2decf
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-11 22:03:51 +0700

    clear unnecessary things

--- src/pages/LandingPage/ClientSection.js
+++ src/pages/LandingPage/ClientSection.js
@@ -3,7 +3,6 @@ import React from 'react';
 import LandingPageStyle from './landingPage.module.scss';
 
 import ClientImages from '../../misc/clientImages';
-import images from '../../assets/clientImages/dompet-dhuafa.svg';
 
 export default function ClientSection() {
   return (
@@ -11,14 +10,15 @@ export default function ClientSection() {
       <div className="text-center">
         <h1>Klien Kami</h1>
         <div className="grid-container">
-          {ClientImages.map(image => {
-            console.log(images);
-            return (
-              <div key={image.id} className={`${LandingPageStyle['client-image-container']}`}>
-                <img src={require('../../assets/clientImages/' + image.src)} alt={image.alt} title={image.title} />
-              </div>
-            );
-          })}
+          {ClientImages.map(image => (
+            <div key={image.id} className={`${LandingPageStyle['client-image-container']}`}>
+              <img
+                src={require(`../../assets/clientImages/${image.src}`)}
+                alt={image.alt}
+                title={image.title}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
