e099269cc07093a156c4df6bd18449428888de2d
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-15 20:55:32 +0700

    fix footer style

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,39 +7,34 @@ import LogoCSAbu from '../../assets/logo_cs_abu.png';
 
 export default function Footer() {
   return (
-    <footer className={`${FooterStyle.footer}`}>
-      <div
-        className={`
-          ${FooterStyle.footerContent} 
-          ${FooterStyle.gridContainer}
-        `}
-      >
-        <div>
+    <footer className={`${FooterStyle['footer-container']}`}>
+      <div className={FooterStyle['footer-content']}>
+        <div className={FooterStyle['footer-logo']}>
           <img src={LogoCSAbu} alt="Logo CleanSheet Abu" />
         </div>
-        <div className={`${FooterStyle.footerElement}`}>
+        <div className={FooterStyle['footer-element']}>
           <h3>Alamat</h3>
-          <div>
+          <div className={FooterStyle['footer-caption']}>
             <p>Taman Dramaga Permai Jalan Rasamala Blok I.11 No.7 Ciampea, Bogor</p>
           </div>
         </div>
-        <div className={`${FooterStyle.footerElement}`}>
+        <div className={FooterStyle['footer-element']}>
           <h3>Sosial Media</h3>
-          <div>
+          <div className={FooterStyle['footer-caption']}>
             <p>Instagram : @cleansheet</p>
             <p>Line : cleansheet</p>
           </div>
         </div>
-        <div className={`${FooterStyle.footerElement}`}>
+        <div className={FooterStyle['footer-element']}>
           <h3>Jam Kerja</h3>
-          <div>
+          <div className={FooterStyle['footer-caption']}>
             <p>Setiap Hari</p>
             <p>24 jam</p>
           </div>
         </div>
-        <div className={`${FooterStyle.footerElement}`}>
+        <div className={FooterStyle['footer-element']}>
           <h3>Kontak</h3>
-          <div>
+          <div className={FooterStyle['footer-caption']}>
             <WhatsappButton />
             <p>085882021749 (Whatsapp)</p>
           </div>
