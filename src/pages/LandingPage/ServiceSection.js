de122bb59ae042de8d064249a4ba4c32f038f668
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-04-16 15:44:09 +0700

    service section responsivities done

--- src/pages/LandingPage/ServiceSection.js
+++ src/pages/LandingPage/ServiceSection.js
@@ -9,41 +9,44 @@ import OutlinedButton from '../../components/Buttons/OutlinedButton';
 
 export default function ServiceSection({ goToServicePage }) {
   return (
-    <section id="services" className="section-gap">
-      <div className="text-center">
-        <h1>Layanan Kami</h1>
-        <div
-          className={`
-            ${LandingPageStyle.gridContainer}
-          `}
-        >
-          <div className={`${LandingPageStyle.serviceDesc}`}>
+    <section
+      id="services"
+      className={`section-gap section-full-height ${LandingPageStyle['service-section']}`}
+    >
+      <h1 className="text-center">Layanan Kami</h1>
+      <div className={LandingPageStyle['service-section-container']}>
+        <div className={LandingPageStyle['service-description']}>
+          <div className={LandingPageStyle['service-image']}>
             <img alt="Layanan Kebersihan" src={CleaningServicesImage} />
-            <h4>Layanan Kebersihan</h4>
-            <p>
-              Cleansheet dapat membersihkan rumah dan kosan apapun sebelum atau sehabis acara sampai
-              pindahan.
-            </p>
           </div>
-          <div className={`${LandingPageStyle.serviceDesc}`}>
+          <h4>Layanan Kebersihan</h4>
+          <p>
+            Cleansheet dapat membersihkan rumah dan kosan apapun sebelum atau sehabis acara sampai
+            pindahan.
+          </p>
+        </div>
+        <div className={LandingPageStyle['service-description']}>
+          <div className={LandingPageStyle['service-image']}>
             <img alt="Layanan Cuci Barang" src={LaundryServicesImage} />
-            <h4>Layanan Cuci Barang</h4>
-            <p>
-              Cleansheet dapat membersihkan barang apapun dari sepatu, tas, karpet, helm sampai
-              kendaraan.
-            </p>
           </div>
-          <div className={`${LandingPageStyle.serviceDesc}`}>
+          <h4>Layanan Cuci Barang</h4>
+          <p>
+            Cleansheet dapat membersihkan barang apapun dari sepatu, tas, karpet, helm sampai
+            kendaraan.
+          </p>
+        </div>
+        <div className={LandingPageStyle['service-description']}>
+          <div className={LandingPageStyle['service-image']}>
             <img alt="Layanan Penanganan Lingkungan" src={EnvironmentCleaningImage} />
-            <h4>Layanan Penanganan Lingkungan</h4>
-            <p>
-              Cleansheet dapat mengatasi permasalahan lingkungan seperti sampah dan lain sebagainya.
-            </p>
           </div>
+          <h4>Layanan Penanganan Lingkungan</h4>
+          <p>
+            Cleansheet dapat mengatasi permasalahan lingkungan seperti sampah dan lain sebagainya.
+          </p>
         </div>
-        <div className={`${LandingPageStyle.buttonMargin5}`}>
-          <OutlinedButton clickAction={goToServicePage} label="Selengkapnya" />
-        </div>
+      </div>
+      <div className={LandingPageStyle['service-section-button']}>
+        <OutlinedButton clickAction={goToServicePage} label="Selengkapnya" />
       </div>
     </section>
   );
