86007ee710681e1742831e1ed8a0dc4381876e40
pramesywaraj <pramesywaraj@pahamify.com>
2021-05-22 16:13:21 +0700

    add build

---
+++ build/precache-manifest.c594701539fed6808a0dcd00f4b15091.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7fdf439dcae990285ea8adf298db0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce84c419adb7db2d511",
+    "url": "/static/css/main.f05f8584.chunk.css"
+  },
+  {
+    "revision": "e02cc231df411785bef0",
+    "url": "/static/js/2.ec0f06cd.chunk.js"
+  },
+  {
+    "revision": "4e47723aef56c4c7d3a45ec806795252",
+    "url": "/static/js/2.ec0f06cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b10531972cab1e28fe3",
+    "url": "/static/js/3.8599fe09.chunk.js"
+  },
+  {
+    "revision": "fb7cc67af055fcd2e07b",
+    "url": "/static/js/4.39a723d8.chunk.js"
+  },
+  {
+    "revision": "dce84c419adb7db2d511",
+    "url": "/static/js/main.da07a436.chunk.js"
+  },
+  {
+    "revision": "41c40f889202f51a7dc6",
+    "url": "/static/js/runtime-main.739efc9a.js"
+  },
+  {
+    "revision": "86c9c0c64d8039d66438cd7efe5c82a4",
+    "url": "/static/media/amura.86c9c0c6.svg"
+  },
+  {
+    "revision": "2387f7d609664b027a05cd9e53a1e9e5",
+    "url": "/static/media/aspherica.2387f7d6.svg"
+  },
+  {
+    "revision": "e7338d0659dbe51b9f970f52f452f302",
+    "url": "/static/media/bpr-dp-taspen.e7338d06.svg"
+  },
+  {
+    "revision": "4bee328332f43e60ca2397a59e228f97",
+    "url": "/static/media/cart.4bee3283.svg"
+  },
+  {
+    "revision": "d8d27c3495d7cedffe3d1c72886b82bc",
+    "url": "/static/media/cleaning_services.d8d27c34.svg"
+  },
+  {
+    "revision": "b801e345b48fcc879ac42d11c952cfa5",
+    "url": "/static/media/cleansheet-owner.b801e345.png"
+  },
+  {
+    "revision": "c12b78bc902b4bbafedd080ba09560a0",
+    "url": "/static/media/dibalik-layar.c12b78bc.svg"
+  },
+  {
+    "revision": "d2218407c82d9c94fe41695f3ca93cac",
+    "url": "/static/media/direktorat-kemahasiswaan-karir-ipb.d2218407.svg"
+  },
+  {
+    "revision": "0ad2e2b4ba3009f642d01e43d6d16d28",
+    "url": "/static/media/dompet-dhuafa.0ad2e2b4.svg"
+  },
+  {
+    "revision": "58b5014fd97c3ac5f952d1ae7dcb8d62",
+    "url": "/static/media/dramaga-tower.58b5014f.svg"
+  },
+  {
+    "revision": "b6b4e62593d693c14ee8e9f304f5bb66",
+    "url": "/static/media/env_cleaning.b6b4e625.svg"
+  },
+  {
+    "revision": "799b0ec631bea1116fc06197adcd12ca",
+    "url": "/static/media/image_default.799b0ec6.svg"
+  },
+  {
+    "revision": "e597098bee518326f3baa4b48805383b",
+    "url": "/static/media/ipb-baru.e597098b.svg"
+  },
+  {
+    "revision": "b1bd55fff5179e30297c02ceda4152fb",
+    "url": "/static/media/kemenpora.b1bd55ff.svg"
+  },
+  {
+    "revision": "afc214aa917a778dcd7a7675e7564ac1",
+    "url": "/static/media/labschool-anak-ipb.afc214aa.svg"
+  },
+  {
+    "revision": "484959a745828c0b6f99cf318ca9767b",
+    "url": "/static/media/landingpage_main_image.484959a7.svg"
+  },
+  {
+    "revision": "e6387b253f454df1c33f999b7589530a",
+    "url": "/static/media/laundry_washing.e6387b25.svg"
+  },
+  {
+    "revision": "43ec898281fa811b78a132c2e6a356a6",
+    "url": "/static/media/logo_cs.43ec8982.png"
+  },
+  {
+    "revision": "8e9b4233235504dd73e1310e0404f91c",
+    "url": "/static/media/mandiri-syariah.8e9b4233.svg"
+  },
+  {
+    "revision": "72ac29dae158a058b4801dc198d7c3dc",
+    "url": "/static/media/masyarakat-mandiri.72ac29da.svg"
+  },
+  {
+    "revision": "2745d24831fdffe0336366f912365d62",
+    "url": "/static/media/olu-coffee-kitchen.2745d248.svg"
+  },
+  {
+    "revision": "c7a6909dcb64cac2bcdcb76efcae8f7f",
+    "url": "/static/media/radar-bogor.c7a6909d.svg"
+  },
+  {
+    "revision": "9fee0b82c7b045b008e40a4c32019dbc",
+    "url": "/static/media/ristekdikti.9fee0b82.svg"
+  },
+  {
+    "revision": "32f38cbbccf998f8d845099d626e59c0",
+    "url": "/static/media/sablon-bogor.32f38cbb.svg"
+  },
+  {
+    "revision": "532b5aba69f6d3dffc4358660dc51bd5",
+    "url": "/static/media/swara-yasmin.532b5aba.svg"
+  },
+  {
+    "revision": "f9aa77a485e135d7b5c99045c674841e",
+    "url": "/static/media/tebar-aqiqah.f9aa77a4.svg"
+  },
+  {
+    "revision": "b5bb326ad31a303cde662099680ec794",
+    "url": "/static/media/tribun-bogor.b5bb326a.svg"
+  },
+  {
+    "revision": "39e15facebd38ebc93d6cd456b169236",
+    "url": "/static/media/weding-design-bogor.39e15fac.svg"
+  },
+  {
+    "revision": "ea0a5ddc73b0add0c0b82ca86f99f9ce",
+    "url": "/static/media/whatsapp.ea0a5ddc.svg"
+  },
+  {
+    "revision": "8ff2b97fef7dfa143bea4042c177c60e",
+    "url": "/static/media/yayasan-bakti-insani.8ff2b97f.svg"
+  }
+]);
