33a1d383748557efdf1333cc1d8e20ce6dc46f70
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-04 16:19:20 +0700

    add demo tag

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import DemoTag from 'components/Tags/DemoTag';
 
 function Layout({ children }) {
+  const [isShowDemoTag, setIsShowDemoTag] = useState(false);
+  
+  useEffect(() => {
+    if (process.env.REACT_APP_DEMO_ONLY === 'true') setIsShowDemoTag(true);
+  }, []);
+
   return (
     <main role="main">
+      {isShowDemoTag && <DemoTag />}
       <Header />
       {children}
       <Footer />
---
+++ src/components/Tags/DemoTag.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const DemoTag = () => {
+  return (
+    <div className={styles["demo-tag-container"]}>
+      <p>This is just a DEMO</p>
+    </div>
+  );
+};
+
+export default DemoTag;
